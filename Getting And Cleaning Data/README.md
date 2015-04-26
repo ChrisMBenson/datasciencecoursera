4b35a39ec25e5a6ca18fd282f753f0ead6717f61
ChrisMBenson <chrisbens@gmail.com>
2015-04-26 11:35:35 +1000

    Initial commit

---
+++ Getting And Cleaning Data/README.md
@@ -0,0 +1,46 @@
+UCI HAR Dataset Analysis
+========================
+
+This repo contains the R scripts that can be used to analysis the [UCI HAR Dataset](http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones) and convert it into a tidy data set.
+
+This was done as the course project for the "Getting and Cleaning Data" course in Coursera which is part of the "Data Science" specialization track.
+
+## Requirements
+
+Create a R script that does the following
+
+- Merges the training and the test sets to create one data set.
+- Extracts only the measurements on the mean and standard deviation for each measurement.
+- Uses descriptive activity names to name the activities in the data set
+- Appropriately labels the data set with descriptive variable names.
+- From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+## Steps to work on this course project
+
+1. Download the data source and put into a folder on your local drive. You'll have a ```UCI HAR Dataset``` folder.
+2. Put ```run_analysis.R``` in the parent folder of ```UCI HAR Dataset```, then set it as your working directory using ```setwd()``` function in RStudio.
+3. Run ```source("run_analysis.R")```, then it will generate a new file ```tiny_data.txt``` in your working directory.
+
+## Dependencies
+
+```run_analysis.R``` file will help you to install the dependencies automatically. It depends on ```reshape2```
+
+## R code
+
+The R code that is used for analysis is available in the [run_analysis.R](run_analysis.R) file.
+
+Source the file in R using the following command and it will automatically download the dataset, perform the transformation, tidy the data and save it in the file `tidy_data.txt`.
+
+```R
+source("run_analysis.R")
+```
+
+The tidy data set can be loaded back into R using the following command
+
+```R
+tidy_data <- read.table("tidy_data.txt")
+```
+
+## Data CodeBook
+
+The [codebook](CodeBook.md) available in this repo describes the variables, the data, the transformations that are done and the clean up that was performed on the data.
---
+++ Getting And Cleaning Data/run_analysis.R
@@ -0,0 +1,77 @@
+
+#Step 1. Merges the training and the test sets to create one data set.
+#Step 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+#Step 3. Uses descriptive activity names to name the activities in the data set
+#Step 4. Appropriately labels the data set with descriptive variable names. 
+#Step 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+
+
+## STEP 0: load required packages
+
+# load the reshape2 package (will be used in STEP 5)
+library(reshape2)
+
+
+## STEP 1: Merges the training and the test sets to create one data set
+
+# Read the training data into data frames
+train_sub <- read.table("train/subject_train.txt")
+train_x <- read.table("train/X_train.txt")
+train_y <- read.table("train/y_train.txt")
+train <- cbind(train_sub, train_y, train_x)
+
+# Read the test data into data frames
+test_sub <- read.table("test/subject_test.txt")
+test_x <- read.table("test/X_test.txt")
+test_y <- read.table("test/y_test.txt")
+test <- cbind(test_sub, test_y, test_x)
+
+#merge the train data and test data sets
+merged_data <- rbind(train, test)
+
+
+
+## STEP 2: Extracts only the measurements on the mean and standard deviation for each measurement.
+
+
+# Load and add column names for measurement files
+feature_names <- read.table("features.txt", col.names = c("id", "name"))
+features <- c("subject", "activity", as.vector(feature_names[, "name"]))
+
+names(merged_data) <- features 
+
+# filter only features that has mean or std in the name
+filter_features <- grepl("mean|std|subject|activity", features) & !grepl("meanFreq", features)
+feature_data = merged_data[, filter_features]
+
+
+## STEP 3: Uses descriptive activity names to name the activities in the data set.
+activities <- read.table("activity_labels.txt", col.names=c("id", "name"))
+for (i in 1:nrow(activities)) {
+  feature_data$activity[feature_data$activity == activities[i, "id"]] <- as.character(activities[i, "name"])
+}
+
+
+## STEP 4: Appropriately labels the data set with descriptive activity names. 
+descriptive_feature_names <- features[filter_features]
+descriptive_feature_names <- gsub("\\(\\)", "", descriptive_feature_names)
+descriptive_feature_names <- gsub("Acc", "-acceleration", descriptive_feature_names)
+descriptive_feature_names <- gsub("Mag", "-Magnitude", descriptive_feature_names)
+descriptive_feature_names <- gsub("^t(.*)$", "\\1-time", descriptive_feature_names)
+descriptive_feature_names <- gsub("^f(.*)$", "\\1-frequency", descriptive_feature_names)
+descriptive_feature_names <- gsub("(Jerk|Gyro)", "-\\1", descriptive_feature_names)
+descriptive_feature_names <- gsub("BodyBody", "Body", descriptive_feature_names)
+descriptive_feature_names <- tolower(descriptive_feature_names)
+
+names(feature_data) <- descriptive_feature_names
+
+
+## STEP 5: Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+# create the tidy data set
+melted <- melt(feature_data, id=c("subject","activity"))
+tidy_data <- dcast(melted, subject+activity ~ variable, mean)
+
+# write the tidy data set to a file
+write.table(tidy_data, "tidy_data.txt", row.names=FALSE)
